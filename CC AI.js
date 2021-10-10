8f7b03087a338e5ad6e09a5694a043d67dfa1104
iasinme <37167550+iasinme@users.noreply.github.com>
2021-10-10 13:19:27 -0600

    Update CC AI.js

--- CC AI.js
+++ CC AI.js
@@ -701,7 +701,7 @@ function downloadStockMarketData() {
 	});
 
 	var hiddenElement = document.createElement('a');
-	hiddenElement.href = 'SMdata:text/csv;charset=utf-8,' + encodeURI(csv);
+	hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
 	hiddenElement.target = '_blank';
 	hiddenElement.download = 'Stock Market.csv';
 	hiddenElement.click();
